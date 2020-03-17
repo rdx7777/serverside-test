48096459cb86a8e5dac4312ff7d274e7c751a0ec
rdx7777 <radek.jerzynski@gmail.com>
2020-03-17 02:07:33 +0000

    added ThymeleafController for basic front-end

--- src/main/java/io/github/rdx7777/serversidetest/controller/ThymeleafController.java
+++ src/main/java/io/github/rdx7777/serversidetest/controller/ThymeleafController.java
@@ -1,5 +1,9 @@
 package io.github.rdx7777.serversidetest.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import io.github.rdx7777.serversidetest.model.WebsiteDataPart;
 import io.github.rdx7777.serversidetest.service.ApplicationService;
 import io.github.rdx7777.serversidetest.service.ServiceOperationException;
@@ -10,27 +14,34 @@ import org.apache.commons.validator.routines.UrlValidator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 
-@RestController
-@RequestMapping("/")
-public class ApplicationController {
+@Controller
+@RequestMapping("/console")
+public class ThymeleafController {
 
-    private final Logger logger = LoggerFactory.getLogger(ApplicationController.class);
+    private final Logger logger = LoggerFactory.getLogger(ThymeleafController.class);
 
-    private ApplicationService applicationService;
+    private final ApplicationService applicationService;
 
-    public ApplicationController(ApplicationService applicationService) {
+    public ThymeleafController(ApplicationService applicationService) {
         this.applicationService = applicationService;
     }
 
-    @GetMapping(params = "address", produces = "application/json")
-    public ResponseEntity<WebsiteDataPart> getData(@RequestParam("address") String address) throws ServiceOperationException {
+    @GetMapping
+    public String showInputAddressForm() {
+        return "input";
+    }
+
+    @SuppressWarnings("DuplicatedCode")
+    @PostMapping(path = "/json", produces = "application/json")
+    public @ResponseBody String showResult(@RequestParam(value = "address", required = false) String address) throws ServiceOperationException, JsonProcessingException {
         UrlValidator validator = new UrlValidator();
         if (!validator.isValid(address)) {
             logger.error("Attempt to get website by providing invalid address.");
@@ -41,6 +52,8 @@ public class ApplicationController {
             logger.error("Attempt to get website data by providing incorrect address for this application.");
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Attempt to get website data by providing incorrect address for this application.");
         }
-        return ResponseEntity.ok(dataPart.get());
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper.writeValueAsString(dataPart.get());
     }
 }
